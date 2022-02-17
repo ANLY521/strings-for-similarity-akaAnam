73cee89cdd5b6ae9cadf63c1463844d3a44580be
akaAnam <ak1963@georgetown.edu>
2022-02-17 02:23:17 -0500

    string similarity

--- README.md
+++ README.md
@@ -10,21 +10,43 @@ Data is from the [STS benchmark](http://ixa2.si.ehu.es/stswiki/index.php/STSbenc
 **TODO:**
 Describe each metric in ~ 1 sentence
 
+NIST: NIST is an extension of the BLEU metric and it assesses the quality of text by calculating n-gram precision scores
+and assigns weights to words based off of importance or rareness. 
+
+BLEU: BLEU calculates n-gram scores of candidate text in a similar way to NIST however it does not assign weights, it only
+counts matches between n-grams. 
+
+WER: Word Error Rate metric is used by calculating the distance between candidate and reference text using Levenshtein Distance.
+Similar to Edit distance
+
+
+LCS: Longest Common Substring metric is calculated by finding the longest common substring between 2 pairs of text. 
+
+Edit Dist: Calculates distance between strings by counting the number of deletions, insertions, and/or substitutions it takes 
+to convert one to the other. All operations are of equal cost
+
 **TODO:** Fill in the correlations. Expected output for DEV is provided; it is ok if your actual result
 varies slightly due to preprocessing/system difference, but the difference should be quite small.
 
 **Correlations:**
 
 Metric | Train | Dev | Test 
------- | ----- | --- | ----
-NIST | (fill me) | 0.593 | (fill me)
-BLEU | (fill me) | 0.433 | (fill me)
-WER | (fill me) | -0.452| (fill me)
-LCS | (fill me) | 0.468| (fill me)
-Edit Dist | (fill me) | -0.175| (fill me)
+------ |-------| --- | ----
+NIST | 0.493 | 0.593 | 0.464
+BLEU | 0.371 | 0.433 | 0.353
+WER | -0.362 | -0.452| -0.364
+LCS | 0.463 | 0.468| 0.504
+Edit Dist | 0.033 | -0.175| -0.039
 
 **TODO:**
 Show usage of the homework script with command line flags (see example under lab, week 1).
+
+ python sts_pearson.py --sts_data stsbenchmark/sts-dev.csv
+
+ python sts_pearson.py --sts_data stsbenchmark/sts-train.csv
+
+ python sts_pearson.py --sts_data stsbenchmark/sts-test.csv
+
 
 
 ## lab, week 1: sts_nist.py
--- sts_pearson.py
+++ sts_pearson.py
@@ -1,6 +1,12 @@
+from difflib import SequenceMatcher
+
+from nltk import word_tokenize, edit_distance
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from nltk.translate.nist_score import sentence_nist
 from scipy.stats import pearsonr
 import argparse
 from util import parse_sts
+from sts_nist import symmetrical_nist
 
 
 def main(sts_data):
@@ -16,13 +22,71 @@ def main(sts_data):
     # HINT: Longest common substring can be complicated. Investigate difflib.SequenceMatcher for a good option.
     score_types = ["NIST", "BLEU", "Word Error Rate", "Longest common substring", "Edit Distance"]
 
+    nist = []
+    bleu = []
+    wer = []
+    lcs = []
+    ed = []
+
+    for word in texts:
+        t1, t2 = word
+
+        # input tokenized text
+        t1_toks = word_tokenize(t1.lower())
+        t2_toks = word_tokenize(t2.lower())
+
+        # try / except for each side because of ZeroDivision Error
+        # 0.0 is lowest score - give that if ZeroDivision Error
+
+        # nist metric
+        try:
+            nist_1 = sentence_nist([t1_toks, ], t2_toks)
+        except ZeroDivisionError:
+            # print(f"\n\n\nno NIST, {i}")
+            nist_1 = 0.0
+
+        nist.append(nist_1)
+
+        # bleu metric
+        # https://machinelearningmastery.com/calculate-bleu-score-for-text-python/
+        # smoothing error: https://www.nltk.org/_modules/nltk/translate/bleu_score.html
+
+        try:
+            bleu_m = sentence_bleu([t1_toks,],t2_toks, smoothing_function = SmoothingFunction().method0)
+        except ZeroDivisionError:
+            bleu_m = 0.0
+        bleu.append(bleu_m)
+
+        # wer metric
+        # formula using the edit dist function
+
+        wer_m = edit_distance(t1_toks,t2_toks)/(len(t1_toks) + len(t2_toks))
+        wer.append(wer_m)
+
+        # LCR metric
+        lcs_m = SequenceMatcher(None,t1,t2).ratio()
+        lcs.append(lcs_m)
+
+        # edit distance metric
+
+        ed_m = edit_distance(t1,t2)
+        ed.append(ed_m)
+
+
+    all_scores = []
+    all_scores.append(nist)
+    all_scores.append(bleu)
+    all_scores.append(wer)
+    all_scores.append(lcs)
+    all_scores.append(ed)
+
     #TODO 3: Calculate pearson r between each metric and the STS labels and report in the README.
     # Sample code to print results. You can alter the printing as you see fit. It is most important to put the results
     # in a table in the README
     print(f"Semantic textual similarity for {sts_data}\n")
-    for metric_name in score_types:
-        score = 0.0
-        print(f"{metric_name} correlation: {score:.03f}")
+    for metric_name in range(0,len(score_types)):
+        score = pearsonr(all_scores[metric_name], labels)[0]
+        print(f"{score_types[metric_name]} correlation: {score:.03f}")
 
     # TODO 4: Complete writeup as specified by TODOs in README (describe metrics; show usage)
 
